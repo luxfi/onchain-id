657f835131b4ee54d41fd9d9033aeccb7eb3467e
Sergi Roca Laguna <sergirocalaguna@Sergis-MacBook-Pro.local>
2025-02-11 18:10:24 +0100

    add naspects and refactor

--- contracts/TopicIdMapping.sol
+++ contracts/TopicIdMapping.sol
@@ -3,13 +3,16 @@ pragma solidity 0.8.17;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-error TopicIdMapping__TopicAlreadyExists();
-
+/// @title A title that should describe the contract/interface
+/// @notice This contract is used to map a claim topic id to its content
+/** @dev this contract stores and returns the stringified version of the content for different topics
+please, check the following link for reference https://docs.onchainid.com/docs/developers/sdk/constants/
+*/
 contract TopicIdMapping is Ownable {
     mapping(uint256 => string) public topicToContent;
 
-    constructor() Ownable() {}
-
+    /// @notice Saves the content for a given topic
+    /// @dev Stores the stringified content for a given topic
     function setTopicContent(
         uint256 _topic,
         string memory _content
@@ -17,6 +20,8 @@ contract TopicIdMapping is Ownable {
         topicToContent[_topic] = _content;
     }
 
+    /// @notice Returns the content for a given topic
+    /// @dev Retrieves the stringified content for a given topic
     function getTopicContent(
         uint256 _topic
     ) external view returns (string memory) {
