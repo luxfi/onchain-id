f2346175a5f94b737dc262feee1c391902dce978
Sergi Roca Laguna <sergirocalaguna@Sergis-MacBook-Pro.local>
2025-05-22 17:30:44 +0200

    add tests

--- contracts/TopicIdMapping.sol
+++ contracts/TopicIdMapping.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.27;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ITopicIdMapping} from "./interface/ITopicIdMapping.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /// @notice This contract is used to map a claim topic id to its name
 /** @dev this contract stores and returns the names for different topics
-*/
-contract TopicIdMapping is Ownable, ITopicIdMapping {
+ */
+contract TopicIdMapping is
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ITopicIdMapping
+{
     mapping(uint256 => Topic) public topicToInfo;
+
+    function initialize() external initializer {
+        __Ownable_init();
+    }
 
     /// @notice Adds a new topic with its format and name
     /// @param _topic The unique identifier for the topic
@@ -19,10 +28,7 @@ contract TopicIdMapping is Ownable, ITopicIdMapping {
         uint256 _format,
         string memory _name
     ) external override onlyOwner {
-        topicToInfo[_topic] = Topic({
-            format: _format,
-            name: _name
-        });
+        topicToInfo[_topic] = Topic({format: _format, name: _name});
 
         emit TopicAdded(_topic, _format);
     }
@@ -36,19 +42,14 @@ contract TopicIdMapping is Ownable, ITopicIdMapping {
         uint256 _format,
         string memory _name
     ) external override onlyOwner {
-        topicToInfo[_topic] = Topic({
-            format: _format,
-            name: _name
-        });
+        topicToInfo[_topic] = Topic({format: _format, name: _name});
 
         emit TopicChanged(_topic, _format);
     }
 
     /// @notice Removes a topic from the mapping
     /// @param _topic The unique identifier of the topic to remove
-    function removeTopic(
-        uint256 _topic
-    ) external override onlyOwner {
+    function removeTopic(uint256 _topic) external override onlyOwner {
         Topic memory topic = topicToInfo[_topic];
         delete topicToInfo[_topic];
         emit TopicRemoved(_topic, topic.format);
@@ -62,4 +63,8 @@ contract TopicIdMapping is Ownable, ITopicIdMapping {
     ) external view override returns (Topic memory _topicInfo) {
         return topicToInfo[_topic];
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
